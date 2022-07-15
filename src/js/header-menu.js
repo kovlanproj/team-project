b71811d5630b36a01404ff2c0b70689cc529b592
Denisiimo <denischorny22@gmail.com>
2022-07-15 21:45:05 +0100

    add menu-transition

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -7,18 +7,25 @@
         animation: document.querySelector('.animation'),
         openAnimation: document.querySelector('.header__menu-open'),
         closeAnimation: document.querySelector('.header__menu-close'),
+        animationClose: document.querySelector('.animation-close'),
+        openAnimationClose: document.querySelector('.header__menu-open'),
+        closeAnimationClose: document.querySelector('.header__menu-close'),
     };
 
     refs.openMenuBtn.addEventListener('click', toggleMenu);
     refs.closeMenuBtn.addEventListener('click', toggleMenu);
     refs.openAnimation.addEventListener('click', toggleAnimation);
-    refs.closeAnimation.addEventListener('click', toggleAnimation);
+    refs.closeAnimationClose.addEventListener('click', toggleAnimation);
+    refs.openAnimationClose.addEventListener('click', toggleAnimationClose);
+    refs.closeAnimationClose.addEventListener('click', toggleAnimationClose);
 
     function toggleMenu() {
-        refs.menu.classList.toggle('is-hidden');
         refs.body.classList.toggle('no-scroll');
     }
     function toggleAnimation() {
         refs.menu.classList.toggle('animation');
+    }
+    function toggleAnimationClose() {
+        refs.menu.classList.toggle('animation-close');
     }
     })();
