0caea9fc56d4d96faf4520663214d1b15eddf1f3
kovlanproj <104130825+kovlanproj@users.noreply.github.com>
2022-07-17 19:45:10 +0300

    made modal

---
+++ src/js/modals.js
@@ -0,0 +1,103 @@
+// !(function (e) {
+//   'function' != typeof e.matches &&
+//     (e.matches =
+//       e.msMatchesSelector ||
+//       e.mozMatchesSelector ||
+//       e.webkitMatchesSelector ||
+//       function (e) {
+//         for (
+//           var t = this,
+//             o = (t.document || t.ownerDocument).querySelectorAll(e),
+//             n = 0;
+//           o[n] && o[n] !== t;
+
+//         )
+//           ++n;
+//         return Boolean(o[n]);
+//       }),
+//     'function' != typeof e.closest &&
+//       (e.closest = function (e) {
+//         for (var t = this; t && 1 === t.nodeType; ) {
+//           if (t.matches(e)) return t;
+//           t = t.parentNode;
+//         }
+//         return null;
+//       });
+// })(window.Element.prototype);
+
+document.addEventListener('DOMContentLoaded', function () {
+  /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+  const modalButtons = document.querySelectorAll('.js-open-modal'),
+    overlay = document.querySelector('.js-overlay-modal'),
+    closeButtons = document.querySelectorAll('.js-modal-close');
+
+  /* Перебираем массив кнопок */
+  modalButtons.forEach(function (item) {
+    /* Назначаем каждой кнопке обработчик клика */
+    item.addEventListener('click', function (e) {
+      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+      e.preventDefault();
+
+      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+      const modalId = this.getAttribute('data-modal'),
+        modalElem = document.querySelector(
+          '.modal[data-modal="' + modalId + '"]'
+        );
+
+      /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+      modalElem.classList.add('active');
+      overlay.classList.add('active');
+      window.addEventListener('keydown', onEscKeyPress);
+      document.body.classList.add('modal-open');
+    }); // end click
+  }); // end foreach
+
+  closeButtons.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      var parentModal = this.closest('.modal');
+
+      parentModal.classList.remove('active');
+      overlay.classList.remove('active');
+      document.body.classList.remove('modal-open');
+    });
+  }); // end foreach
+
+  // document.body.addEventListener(
+  //   'keyup',
+  //   function (e) {
+  //     var key = e.keyCode;
+
+  //     if (key == 27) {
+  //       document.querySelector('.modal.active').classList.remove('active');
+  //       document.querySelector('.overlay').classList.remove('active');
+  //     }
+  //   },
+  //   false
+  // );
+
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+      console.log(document.querySelector('.modal-overlay'));
+      document.querySelector('.modal.active').classList.remove('active');
+      document
+        .querySelector('.modal-overlay.active')
+        .classList.remove('active');
+      document.body.classList.remove('modal-open');
+      window.removeEventListener('keydown', onEscKeyPress);
+    }
+  }
+
+  overlay.addEventListener('click', function () {
+    document.querySelector('.modal.active').classList.remove('active');
+    this.classList.remove('active');
+    document.body.classList.remove('modal-open');
+  });
+}); // end ready
